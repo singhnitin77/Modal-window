6f8607e4167dca4ac8fb4e7a73818dd35f3830ef
Nitin Singh <nitinsinghofficial9@gmail.com>
2022-02-23 19:51:53 +0530

    Deleted comments

--- script2.js
+++ script2.js
@@ -7,11 +7,6 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
-/* 
-Limitation of query selector method , whenever we use query selector with a selector, which matches with multiple elements, only the first one will get selected
-So we need to use querySelectorALL 
-*/
-
 const openModal = function () {
   //   console.log('Button clicked');
   modal.classList.remove('hidden');
@@ -49,16 +44,6 @@ const closeModal = function () {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
-
-// Keypress events
-/* 
-Keyboard events are so-called global events because they don't happen on one specific element. 
-For global events like Keyboard events we list them on the whole document.
-There are 3 types of events for the keyboards
-Keydown
-keypress
-keyup - Only happens when we lift our fingers off the key
-*/
 
 document.addEventListener('keydown', function (e) {
   //   console.log(e);
